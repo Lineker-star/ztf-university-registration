5cb7d158cbb42e980951cc1c5d97d531c3d5aba1
Eker - Megaj Technologies <megajtechnologies@gmail.com>
2026-06-30 00:17:05 +0100

    fix: use standalone server.js for PM2 on Hostinger

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,16 @@
 module.exports = {
   apps: [{
     name: 'ztf-registration',
-    script: 'node_modules/.bin/next',
-    args: 'start',
+    script: '.next/standalone/server.js',
     cwd: '/home/u161800917/domains/register.ztfuniversity.com/nodejs',
     env: {
       NODE_ENV: 'production',
       PORT: 3000,
+      HOSTNAME: '0.0.0.0',
     },
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '512M',
   }],
-}
+};
